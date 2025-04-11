bcdc83626f5d59d42f342ada90acd72349f0a585
Abdurrahman <abdurrahmanuni909@gmail.com>
2025-04-11 23:49:15 +0300

    add camera node, has tested

---
+++ build/AirDefenseUITest/CMakeFiles/AirDefenseUITest_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AirDefenseUITest_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/AirDefenseUITest_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
