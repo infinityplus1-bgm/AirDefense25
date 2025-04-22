adaacdb11135dc0a8f01d0dabad41124577a7a95
Abdurrahman <abdurrahmanuni909@gmail.com>
2025-04-22 21:57:27 +0300

    Resolved merge conflict and updated tracker

--- image processing/tracker.py
+++ image processing/tracker.py
@@ -1,28 +1,26 @@
-<<<<<<< HEAD
 from ultralytics import YOLO
 import cv2 as cv
 import numpy as np
 
 class tracker:
-    def __init__(self, model, video_source, tracker, weights):
+    def __init__(self, model, tracker, weights):
         """Initialize YOLO model, video source, and tracker."""
         self.raw_model = model
-        self.model = model(weights) # TODO:  i don't think the model needs weight check how SORT what does before by @abd eid and implement(done)
-        # TODO:  change this because this takes ready detection from topic (done)
+        self.model = model(weights)  # Assuming model doesn't need weight check
         
+        # Use detection data instead of video capture, remove video source handling
         self.mot_tracker = tracker
         
     def track_objects(self, detections: np.ndarray) -> np.ndarray:
-        #TODO: instead of taking a full frame you will be getting the detection in the format (x1,y1,x2,y2,conf) so change accordingly (done)
+        # Use detection data directly instead of full frames
         if detections is None or len(detections) == 0:
             return np.empty((0, 5), dtype=np.float32)
         tracks = self.mot_tracker.update(detections)
         return np.array(tracks, dtype=np.float32)
-        
+
     def process_frame(self, tracked_objects: np.ndarray):
         """Process the array of tracked objects to get centers and track IDs."""
         centers_with_ids = []
-        # TODO : we don't need to write to the frame as it only published the tracker results and the closest distance to the kill zone (done)
         for obj in tracked_objects:
             x1, y1, x2, y2, track_id = obj
             xa = int((x1 + x2) / 2)  # X center of the bounding box
@@ -30,64 +28,3 @@ class tracker:
             centers_with_ids.append([xa, ya, track_id])  # Include track_id
 
         return np.array(centers_with_ids, dtype=np.int32)
-
-
-
-
-
-"""
-NOTES: 
-i removed the color detection stuff because this is a the tracking node not the detection node and hence we don't want any detections here
-removed the opencv stuff because this node takes all the detection from another node and doesn't detect stuff itself
-please update the track method to include the correct tracking behaviour i tried to look for a function with the name "track_objects" but couldn't find any so please fix it
-"""
-=======
-from ultralytics import YOLO
-import cv2 as cv
-import numpy as np
-
-class tracker:
-    def __init__(self, model, video_source, tracker, weights):
-        """Initialize YOLO model, video source, and tracker."""
-        self.raw_model = model
-        self.model = model(weights) # TODO:  i don't think the model needs weight check how SORT what does before by @abd eid and implement
-        # TODO:  change this because this takes ready detection from topic
-        self.video = video_source 
-        self.video_capture = cv.VideoCapture(video_source)
-        if not self.video_capture.isOpened():
-            print("Error: Video file not opened.")
-            return
-        
-
-        self.mot_tracker = tracker
-        
-    def track_objects(self, frame):
-        #TODO: instead of taking a full frame you will be getting the detection in the format (x1,y1,x2,y2,conf) so change accordingly
-        """Track objects using the SORT tracker per frame."""
-        results = self.model.track(frame, persist=True, tracker=self.mot_tracker, conf=0.53, iou=0.3, agnostic_nms=True, classes=[0])
-        return results
-        
-    def process_frame(self, frame):
-        tracker_results = self.track_objects(frame)
-
-        # TODO : we don't need to write to the frame as it only published the tracker results and the closest distance to the kill zone
-        for r in tracker_results:
-            for box in r.boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                track_id = int(box.id[0])
-                conf = float(box.conf[0])
-
-                cv.putText(frame, f'ID {int(track_id)}', (int(x1), int(y1) - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (10, 3, 7), 1)
-            return frame
-
-
-
-
-
-"""
-NOTES: 
-i removed the color detection stuff because this is a the tracking node not the detection node and hence we don't want any detections here
-removed the opencv stuff because this node takes all the detection from another node and doesn't detect stuff itself
-please update the track method to include the correct tracking behaviour i tried to look for a function with the name "track_objects" but couldn't find any so please fix it
-"""
->>>>>>> b352603 (yolo_detection node need to check by anas)
