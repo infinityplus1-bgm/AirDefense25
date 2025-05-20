30611e527bc124d7dabac19265ce2d404ef3d8b2
infinityplusone <infinityplus1bgm@gmail.com>
2025-05-20 20:51:42 +0300

    added basic functionality lacking some functionality

--- src/main/setup.py
+++ src/main/setup.py
@@ -23,7 +23,9 @@ setup(
             "camera = main.camera.camera:main",
             "camera_test = main.camera.camera_test:main",
             "yolo_detection = main.detection.yolo_detection:main",
-            "detections_overlay_test = main.detection.detections_overlay_test:main"
+            "detections_overlay_test = main.detection.detections_overlay_test:main",
+            "serial = main.serial.serial:main",
+            "serial_test = main.serial.serial_test:main"
         ],
     },
 )
